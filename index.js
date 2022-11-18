f19556e5ae09c63ca6268f31f51ddf3f000a3c43
Shawna White <sw1056@gmail.com>
2022-11-18 14:05:28 -0600

    added other employee prompts array

--- index.js
+++ index.js
@@ -18,23 +18,23 @@ const managerPrompts = () => {
     return inquirer.prompt ([
         {
             type: 'input',
-            message: "Please enter the team manger's name:",
-            name: 'managerName'
+            name: 'name',
+            message: "Please enter the team manger's name:"   
         },
         {
             type: 'input',
-            message: "What is the manager's employee ID?",
-            name: 'id'
+            name: 'id',
+            message: "What is the manager's employee ID?"
         },
         {
             type: 'input',
-            message: "What is the manager's email address:",
-            name: 'email'
+            name: 'email',
+            message: "What is the manager's email address:"
         },
         {
             type: 'input',
-            message: "What is the manager's office number?",
-            name: 'officeNumber'
+            name: 'officeNumber',
+            message: "What is the manager's office number?"
         },
     ])
     .then(managerInput => {
@@ -45,3 +45,48 @@ const managerPrompts = () => {
         console.log(manager);
     })
 };
+
+const otherEmployeePrompts = () => {
+    return inquirer.prompt ([
+        {
+            type: 'list',
+            name: 'role',
+            message: "What is this employee's role on the team?",
+            choices: ['Engineer', 'Intern']
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is this employee's name?"
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is this employee's ID?"
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is this employee's email address?"
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is this engineer's GitHub username?",
+            when: (input) => input.role === "Engineer"
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What school does this intern attend?",
+            when: (input) => input.role === "Intern"
+        },
+        {
+            type: 'confirm',
+            name: 'continueAdding',
+            message: "Would you like to add more team members?",
+            default: false
+        },
+    ])
+    
+}
