b2b20f84edc9d18201142abcb012fe0369e70f4d
Shawna White <sw1056@gmail.com>
2022-11-18 11:49:04 -0600

    wrote employee and engineer tests

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,31 @@
+const Employee = require('../lib/Employee');
+
+describe("Employee", () => {
+    it("should return the name passed in", () => {
+        const newEmp = new Employee("Alex", "5", "alex@company.org");
+        const testName = newEmp.getName();
+
+        expect(testName).toBe("Alex");
+    });
+
+    it("should return the ID passed in", () => {
+        const newEmp = new Employee("Alex", "5", "alex@company.org");
+        const testId = newEmp.getId();
+    
+        expect(testId).toBe("5");
+    });
+    
+    it("should return the email passed in", () => {
+        const newEmp = new Employee("Alex", "5", "alex@company.org");
+        const testEmail = newEmp.getEmail();
+    
+        expect(testEmail).toBe("alex@company.org");
+    });
+    
+    it("should return the role passed in", () => {
+        const newEmp = new Employee("Alex", "5", "alex@company.org");
+        const testRole = newEmp.getRole();
+    
+        expect(testRole).toBe("Employee");
+    });
+});
